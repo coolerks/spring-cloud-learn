9011dbb820e08518fb1596609daaa944db8b13ec
moyok <songxiaoxu2002@gmail.com>
2022-08-27 10:15:32 +0800

    服务集群搭建

--- cloud-eureka-server7002/src/main/java/com/xiaoxu/server7002/EurekaServer7002Application.java
+++ cloud-eureka-server7002/src/main/java/com/xiaoxu/server7002/EurekaServer7002Application.java
@@ -1,4 +1,4 @@
-package com.xiaoxu.eureka;
+package com.xiaoxu.server7002;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,9 +6,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaApplication {
+public class EurekaServer7002Application {
     public static void main(String[] args) {
-        SpringApplication.run(EurekaApplication.class, args);
-
+        SpringApplication.run(EurekaServer7002Application.class, args);
     }
 }
--- cloud-provider-payment8001/src/main/java/com/xiaoxu/cloud/PaymentApplication.java
+++ cloud-provider-payment8001/src/main/java/com/xiaoxu/cloud/PaymentApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class Application {
+public class PaymentApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(PaymentApplication.class, args);
     }
 }
