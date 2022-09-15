77351e686adbf95eea17b4188544a35a9096c889
Moyok <songxiaoxu2002@gmail.com>
2022-09-15 12:10:05 +0800

    Sentinel热点key

--- cloud-alibaba-sentinel-service8401/src/main/java/com/xiaoxu/cloud/controller/MyController.java
+++ cloud-alibaba-sentinel-service8401/src/main/java/com/xiaoxu/cloud/controller/MyController.java
@@ -1,10 +1,9 @@
 package com.xiaoxu.cloud.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/sentinel")
@@ -27,5 +26,16 @@ public class MyController {
         log.info("sleep方法，请求已进入");
         Thread.sleep(time * 1000);
         return "休眠" + time + "秒";
+    }
+
+    @GetMapping("/hotkey")
+    @SentinelResource(value = "hotkey", blockHandler = "hotKeyHandler")
+    public String hotKey(@RequestParam(value = "content", required = false) String content, @RequestParam(value = "param", required = false) String param) {
+        return "hotkey - content = " + content + ",  param = " + param;
+    }
+
+    public String hotKeyHandler(String content, String param, BlockException exception) {
+        return "hotkey - content = " + content + ",  param = " + param + ":(";
+
     }
 }
